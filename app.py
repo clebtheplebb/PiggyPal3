47804a94ee0935857da4b189e489cafcd3592f36
Caleb Chang <changcaleb2007@gmail.com>
2025-04-06 06:50:15 -0700

    update port
    port 8000

--- app.py
+++ app.py
@@ -937,4 +937,4 @@ def internal_error(error):
                          error_message='An unexpected error has occurred.'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, port=8000) 
